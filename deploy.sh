abcec80265b7604c4c0be683dec993e46db682a1
benoit <benoit.goethals@gmail.com>
2026-02-14 17:39:15 +0100

    added deploy

--- deploy.sh
+++ deploy.sh
@@ -50,7 +50,7 @@ sudo docker run -d \
     -p "${PORT}:${PORT}" \
     --restart unless-stopped \
     -e APP_ENV=docker \
-    -v "${CONFIG_DIR}":/etc/CrossClientAPI \
+    -v /home/benoit/path/to/config.yml:/etc/CrossClientAPI/config.yml \
     "${IMAGE_NAME}"
 
 echo ""
